b0ed57c2529b8f812394ec101316d989ec65403d
Ahmad <mahmad@mgc.mentorg.com>
2020-06-14 13:56:27 +0500

    base ]

---
+++ schedular.php
@@ -0,0 +1,38 @@
+<?php require_once __DIR__.'/vendor/autoload.php';
+
+use GO\Scheduler;
+
+// Create a new scheduler
+$scheduler = new Scheduler();
+//$scheduler->php('D:\xampp\htdocs\test2\index.php')->everyMinute();;
+$scheduler->raw('cd c:/xampp/htdocs/resource_planner && php artisan import',[],'RMO')->inForeground()->everyMinute();
+$scheduler->raw('cd c:/xampp/htdocs/support_sla && php artisan sync:database',[],'SUPPORT')->inForeground()->everyMinute();
+$scheduler->raw('cd c:/xampp/htdocs/sos && php artisan sync:calendar',[],'Sprint Calendar')->inForeground()->hourly();
+
+
+// ... configure the scheduled jobs (see below) ...
+
+// Let the scheduler execute jobs which are due.
+$i=1;
+while(1)
+{
+	
+	echo "\e[104m"."Run #".$i++."\e[0m\r\n";
+	$jobs = $scheduler->run();
+	foreach($jobs as $job)
+	{
+		echo "\e[92mJob ".$job->getId()."\e[0m\r\n";
+		//echo "Updating ".$job->getId()."\r\n";
+		$output = $job->getOutput();
+		if($output)
+			foreach($output as $message)
+				echo $message."\r\n";
+		
+	}
+	echo "Finished\r\n"; 
+	
+	//$scheduler->clearJobs();
+	//var_dump($jobs);
+	sleep(60);
+}
+
---
+++ start.bat
@@ -0,0 +1,1 @@
+php schedular.php
